89c6865571834505631faae442f818721ca3e52e
Kadauchi <kadauchi@gmail.com>
2019-07-28 12:56:32 -0700

    Updated dist functions to take an arg instead of separate files.

---
+++ npm.dist.js
@@ -0,0 +1,43 @@
+const fs = require('fs-extra');
+const path = require('path');
+const commandLineArgs = require('command-line-args');
+const JSZip = require('jszip');
+
+const readdirSyncRecursive = require('./readdirSyncRecursive');
+
+const filePaths = readdirSyncRecursive('./build');
+const optionDefinitions = [{ name: 'targets', alias: 't', type: String, multiple: true, defaultOption: true }];
+const options = commandLineArgs(optionDefinitions);
+const package = fs.readJsonSync('./package.json');
+
+fs.ensureDirSync('./dist');
+
+options.targets.forEach((target) => {
+  const zip = new JSZip();
+  const zipName = `${target}-${package.version}`;
+  const zipPath = `./dist/${zipName}.zip`;
+
+  fs.removeSync(zipPath);
+
+  switch (target) {
+    case 'chrome':
+      filePaths.forEach((filePath) => {
+        const parsed = path.parse(filePath);
+        const dirs = parsed.dir.split(path.sep).filter((dir) => dir !== 'build');
+        zip.file(path.join(zipName, ...dirs, parsed.base), fs.readFileSync(filePath));
+      });
+      break;
+    case 'firefox':
+      filePaths.forEach((filePath) => {
+        const parsed = path.parse(filePath);
+        const dirs = parsed.dir.split(path.sep).filter((dir) => dir !== 'build');
+        zip.file(path.join(...dirs, parsed.base), fs.readFileSync(filePath));
+      });
+      break;
+  }
+
+  zip
+    .generateNodeStream({ type: 'nodebuffer', streamFiles: true })
+    .pipe(fs.createWriteStream(zipPath))
+    .on('finish', () => console.log(`${zipPath} created.`));
+});
