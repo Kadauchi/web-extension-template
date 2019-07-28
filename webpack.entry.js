6584cde260ba03494d3f381e462cc5074d90d450
Kadauchi <kadauchi@gmail.com>
2019-07-28 14:42:38 -0700

    Added content_scripts as entries.

--- webpack.entry.js
+++ webpack.entry.js
@@ -1,16 +1,27 @@
-const fs = require('fs-extra');
 const path = require('path');
 
-// Create an entry for every ts and tsx file in the /src/pages folder.
-module.exports = fs.readdirSync('./src/pages').reduce((acc, fileName) => {
-  const [name, ext] = fileName.split('.');
+const readdirSyncRecursive = require('./readdirSyncRecursive');
 
-  if (ext && ext.match(/ts(x?)/)) {
+// Create an entry for every .ts and .tsx file in the src/pages folder.
+const pages = readdirSyncRecursive('src/pages').reduce((acc, file) => {
+  if (file.name.match(/ts(x?)/)) {
     return {
       ...acc,
-      [name]: path.join(__dirname, 'src', 'pages', fileName),
+      [file.name.split('.')[0]]: file.fullPath,
     };
   }
 
   return acc;
 }, {});
+
+// Create an entry for every .ts file in the src/content_scripts folder.
+const content_scripts = readdirSyncRecursive('src/content_scripts').reduce((acc, file) => {
+  if (file.name.match(/\.ts$/)) {
+    return {
+      ...acc,
+      ['content_scripts/' + file.name.split('.')[0]]: file.fullPath,
+    };
+  }
+}, {});
+
+module.exports = { ...pages, ...content_scripts };
